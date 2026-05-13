8014315ea416f1f5c281e1d012126995fbf018e1
Radianceblue <radic.marina91@gmail.com>
2026-05-13 10:45:14 +0200

    Fixed error

--- src/stack.js
+++ src/stack.js
@@ -14,6 +14,7 @@ exports.pop = function() {
 
 // Returnerar det översta elementet i stacken
 exports.peek = function() {
-    return stack[0];// Det här är medvetet felaktigt
+    //return stack[0]; Det här är medvetet felaktigt
+    return _.last(stack);
 }
 
